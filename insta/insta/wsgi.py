fdb4e21dafac31e833f0bd43e15859e425f3b262
nahsiloh <lishan1512@gmail.com>
2019-12-18 15:46:49 +0800

    changed to module insta.settings

--- insta/insta/wsgi.py
+++ insta/insta/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "insta.insta.settings"
+os.environ["DJANGO_SETTINGS_MODULE"] = "insta.settings"
 
 from django.core.wsgi import get_wsgi_application
 
